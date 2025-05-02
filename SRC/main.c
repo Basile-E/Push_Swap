76f1c49f768a7613ecf1c59bf35e2092845e96e8
Basile-E <basile.vennin@gmail.com>
2025-05-02 17:34:41 +0200

    fonction de base ok

--- SRC/main.c
+++ SRC/main.c
@@ -7,10 +7,12 @@ typedef struct s_stack {
     struct s_stack* next; 
 }   t_stack;
 
-void	lst_print(t_stack *head)
+int	lst_print(t_stack *head)
 {
     t_stack * temp;
     temp = head;
+    if (temp == NULL)
+        return(ft_printf("liste Vide\n"));
 
     while (temp)
     {
@@ -54,16 +56,77 @@ void	lstadd_back(t_stack **lst, t_stack *new)
 		lstlast(*lst)->next = new;
 }
 
-
-
-
-
-
-
-
-int main(int ac, char **av)
+int pushTo(t_stack **head_from, t_stack **head_to)
 {
-    t_stack *head = NULL;
+    t_stack *temp;
+    
+    if (!*head_from)
+        return (1);
+        
+    temp = *head_from;
+    *head_from = (*head_from)->next;
+    temp->next = NULL;
+    
+    lstadd_front(head_to, temp);
+    return (0); // Success
+}
+
+int rotate(t_stack **head_to_rotate)
+{
+    t_stack *first;
+    t_stack *last;
+
+    if (!*head_to_rotate || !(*head_to_rotate)->next)
+        return (0);
+
+    first = *head_to_rotate;
+    last = lstlast(*head_to_rotate);
+
+    *head_to_rotate = first->next; 
+
+    first->next = NULL;
+    last->next = first;
+    return (1);
+}
+
+int revers_rotate(t_stack **head_to_rotate)
+{
+    t_stack *first;
+    t_stack *last;
+    t_stack *second_to_last;
+
+    if (!*head_to_rotate || !(*head_to_rotate)->next)
+        return (1);
+    first   = *head_to_rotate;
+    last    = lstlast(*head_to_rotate);
+    second_to_last = first;
+    while (second_to_last->next != last)
+        second_to_last = second_to_last->next;
+    second_to_last->next = NULL;
+    last->next = first;
+    *head_to_rotate = last;
+    return (0);
+}
+
+int swap(t_stack **head_to_swap)
+{
+    t_stack *first;
+    t_stack *second;
+    
+    if (!*head_to_swap || !(*head_to_swap)->next)
+        return (1);
+    first = *head_to_swap;
+    second = first->next;
+    first->next = second->next;
+    second->next = first;
+    *head_to_swap = second;
+    return (0);
+}
+
+int     main(int ac, char **av)
+{
+    t_stack *head_a = NULL;
+    t_stack *head_b = NULL;
     t_stack *temp;
     char    **tableau;
     int     *tab_int;
@@ -117,7 +180,7 @@ int main(int ac, char **av)
             temp = lstnew(tab_int[i]);
             if(!temp)
                 return(ft_printf("Error during linked list init\n"));
-            lstadd_back(&head, temp);
+            lstadd_back(&head_a, temp);
             i++;
         }
     }
@@ -127,22 +190,44 @@ int main(int ac, char **av)
         while (av[i])
         {
             temp = lstnew(ft_atoi(av[i]));
-            lstadd_back(&head, temp);
+            lstadd_back(&head_a, temp);
             i++;
         }
     }
     else if (ac < 2)
         return (ft_printf("no arg error\n"));
 
-    lst_print(head);
+    lst_print(head_a);
+    lst_print(head_b);
+
+    pushTo(&head_a, &head_b);
+    pushTo(&head_a, &head_b);
+    pushTo(&head_a, &head_b);
+
+    lst_print(head_a);
+    lst_print(head_b);
+
+    swap(&head_a);
+
+    lst_print(head_a);
+    lst_print(head_b);
+
+    rotate(&head_a);
+
+    lst_print(head_a);
+    lst_print(head_b);
+    
+    revers_rotate(&head_a);
+
+    lst_print(head_a);
+    lst_print(head_b);
 
     // jusque ici je prend en charge 2 ou plus ac et je crée ma liste chainé proprement
     // il faut maintenant un mini sort si la liste fait moins 4 éléments 
     // et potentiellement commencer a tout envoyer dans leurs fichiers réspéctifs
     // faire un fichier fonctions lst pour pouvoir supprimer celle de la libft
     // et clean un peut le main des fonction de gestion des arg
-    
-    
+
     free(tab_int);
     return 0;
 }
@@ -150,17 +235,5 @@ int main(int ac, char **av)
 
 /*
     Pseudo code :
-
-    main(ac, av)
-
-    verrifier le nombre d'arguments 
-    return error si inferieur a 2 et si = a 2 fait ft_split
-    si supperieur a 2, crée tab_int depuis les arg 
-
-    avec tab_int prêt on peut faire une fonction qui init_lst qui crée la liste
-    grace au elements de tab_int
-
-    en fonction de la taille de la liste faire un mini_sort
-
 
 */
